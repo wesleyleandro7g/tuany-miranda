8f4ebebe6d7a11db9672cee375c82687ba067b09
wesley leandro <wesleyleandro.dev@gmail.com>
2023-03-16 22:02:07 -0300

    Updated video player

--- src/app/(campaigns)/(fashion-mirror)/espelho-da-moda-b/page.tsx
+++ src/app/(campaigns)/(fashion-mirror)/espelho-da-moda-b/page.tsx
@@ -15,7 +15,7 @@ export default function FashionMirrorV1() {
 
   return (
     <main className={montserrat.className}>
-      <section className="min-h-screen relative flex flex-col items-center">
+      <section className="min-h-screen md:w-full relative flex flex-col items-center lg-[1200px]">
         <Image src="/banner.png" alt="Closet" fill className="-z-10" />
         <Countdown />
 
@@ -27,40 +27,13 @@ export default function FashionMirrorV1() {
             </span>
           </h1>
 
-          <div className="relative w-[840px] aspect-video rounded-xl md:rounded-2xl border-vermilion border-2 md:border-4 overflow-hidden">
-            <ReactPlayer
-              url="/espelho-da-moda-vsl-v1.mp4"
-              width="100%"
-              height="100%"
-              className="absolute"
-              playing={playing}
-              muted={false}
-            />
-
-            <Image
-              src="/img01.svg"
-              alt="Closet"
-              fill
-              className={`absolute z-10 opacity-${playing ? 0 : 1}`}
-            />
-            <button
-              onClick={playPause}
-              className={`w-full h-full bg-black absolute z-20 flex justify-center items-center opacity-50`}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="#fff"
-                className="w-24 h-24 cursor-pointer animate-pulse"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-          </div>
+          <iframe
+            id="panda-24825bd7-5f4d-47f1-adc6-dc11b9bf45ab"
+            src="https://player-vz-010c908f-347.tv.pandavideo.com.br/embed/?v=24825bd7-5f4d-47f1-adc6-dc11b9bf45ab"
+            className="border-none rounded-xl aspect-video w-full md:w-[720px] shadow-xl"
+            allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture"
+            allowFullScreen={true}
+          ></iframe>
 
           <h1 className="text-xl text-white text-center font-medium">
             Descubra as técnicas por trás de uma{" "}
--- src/app/(campaigns)/(fashion-mirror)/espelho-da-moda/page.tsx
+++ src/app/(campaigns)/(fashion-mirror)/espelho-da-moda/page.tsx
@@ -4,23 +4,17 @@ import { useState } from "react";
 import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import Countdown from "@/components/countdown";
-import ReactPlayer from "react-player";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function FashionMirrorV1() {
-  const [playing, setPlaying] = useState(false);
-
-  const playPause = () => setPlaying(!playing);
-
   return (
     <main className={montserrat.className}>
-      <section className="min-h-screen md:w-[900px] relative flex flex-col items-center lg-[1200px]">
+      <section className="min-h-screen md:w-full relative flex flex-col items-center lg-[1200px]">
         <Image src="/banner.png" alt="Closet" fill className="-z-10" />
         <Countdown />
 
         <div className="flex flex-col items-center gap-8 px-4 py-8 md:py-12 md:max-w-[900px]">
-          <h1>AQUI!</h1>
           <h1 className="text-2xl text-white text-center font-bold lg:text-4xl">
             Aprenda como transmitir todo o seu potencial através da sua imagem e{" "}
             <span className="text-violet-web">
@@ -28,42 +22,13 @@ export default function FashionMirrorV1() {
             </span>
           </h1>
 
-          <div className="relative w-[840px] aspect-video rounded-xl md:rounded-2xl border-vermilion border-2 md:border-4 overflow-hidden">
-            <ReactPlayer
-              url="/espelho-da-moda-vsl-v1.mp4"
-              width="100%"
-              height="100%"
-              className="absolute"
-              playing={playing}
-              muted={false}
-            />
-
-            <Image
-              src="/img01.svg"
-              alt="Closet"
-              fill
-              className={`absolute z-10 opacity-${playing ? 0 : 1}`}
-            />
-            <button
-              onClick={playPause}
-              className={`w-full h-full bg-black absolute z-20 flex justify-center items-center opacity-${
-                playing ? 0 : 50
-              }`}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="#fff"
-                className="w-24 h-24 cursor-pointer animate-pulse"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-          </div>
+          <iframe
+            id="panda-24825bd7-5f4d-47f1-adc6-dc11b9bf45ab"
+            src="https://player-vz-010c908f-347.tv.pandavideo.com.br/embed/?v=24825bd7-5f4d-47f1-adc6-dc11b9bf45ab"
+            className="border-none rounded-xl aspect-video w-full md:w-[720px] shadow-xl"
+            allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture"
+            allowFullScreen={true}
+          ></iframe>
 
           <h1 className="text-xl text-white text-center font-medium">
             Descubra as técnicas por trás de uma{" "}
